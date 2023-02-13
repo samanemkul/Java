43fc7e3f246d25960ceddb919e3850e0bb97bbdd
samanemkul <nemkulsama1@gmail.com>
2023-02-13 20:37:17 +0545

    added

---
+++ Sama.java
@@ -0,0 +1,18 @@
+public class Sama
+{
+ public static void main(String[] args){
+ 	System.out.println("Name of the organization: DevilsGrill Restaurant\n\n");
+    System.out.println("Address:");
+    System.out.println("----State: KTM\n----Zip Code: 44600\n----Country: Nepal\n\n");
+    System.out.println("Contact Information:\n----Mobile Number: 9840783698\n----Email: Devilsgrill@gmail.com");
+
+ }
+}
+
+
+
+
+
+
+
+
