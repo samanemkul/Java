c5b5e99234c0b4fb11ed9d05eac7c6ae935a754a
samanemkul <nemkulsama1@gmail.com>
2023-02-28 11:23:58 +0545

    Added

---
+++ class_obj/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Shape object = new Shape(50,100);
+//        System.out.println("Width of shape "+object.width);//brings the object of another class
+//        System.out.println("Height of shape "+object.height);
+//       //System.out.println("Area of shape "+object.Area);
+        //object.area();
+        Shape object2 = new Shape(100,200);
+        Shape obj = new Shape();
+    }
+}
---
+++ class_obj/src/Shape.java
@@ -0,0 +1,19 @@
+public class Shape {
+   public int width;
+    public int height;
+   // Default constructor
+    Shape(){
+        width = 10;
+        height = 20;
+    }
+   //Parameterized constructor
+    Shape(int w, int h) {
+       width = w;
+       height = h;
+   }
+//    public void area(){
+//        int area = width*height;
+//        System.out.println("Area "+area);
+//    }
+
+}
